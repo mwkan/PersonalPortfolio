efcc97319bb5e4828ae4735a3f86a27a7bd30e55
Mun Wei Kan <munny.kan@hotmail.co.uk>
2024-05-03 16:40:15 +0100

    create portfolio model in DB

--- personalportfolio/portfolio/models.py
+++ personalportfolio/portfolio/models.py
@@ -6,3 +6,4 @@ class Project(models.Model):
     title = models.CharField(max_length=100)
     description = models.CharField(max_length=250)
     image = models.ImageField(upload_to='portfolio/images/')
+    url = models.URLField(blank=True)
